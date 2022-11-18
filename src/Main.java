03dd6cab011bafe2537a8690b4437e79f51a0c43
Stikender <nadik17@bk.ru>
2022-11-18 12:14:35 +0800

    Finish exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+    public static void printIsLeapYear(int year) {
+        boolean leapYear = isLeapYear(year);
+        printIsLeapYearResult(year, leapYear);
+    }
+
+    private static boolean isLeapYear(int year) {
+        return year % 4 == 0;
+    }
+
+    private static void printIsLeapYearResult(int year, boolean leapYear) {
+        if (leapYear) {
+            System.out.println(year + " — високосный год.");
+        } else {
+            System.out.println(year + " — не високосный год");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Exercise 1
+        int year = 2022;
+        printIsLeapYear(year);
+
+        //Exercise 2
+
+
+
     }
 }
